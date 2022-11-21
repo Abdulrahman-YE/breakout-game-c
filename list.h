2b1b78616de49245cff6fb5a4a9c9d30ce2ec6ad
Abdulrahman-YE <abdulrahman1ye@gmail.com>
2022-11-21 12:57:24 +0300

    Implement basic list defination

---
+++ list.h
@@ -0,0 +1,125 @@
+
+
+#include <stdbool.h>
+
+#include <result.h>
+
+/**
+ * List data.
+*/
+typedef struct List_C LIST_C;
+
+/**
+ * List iterator
+*/
+typedef struct List_Iter List_Iter;
+
+/**
+ * Create new list
+ * @param list
+ *      Create list
+ * @returns
+ *  SUCCESS on success
+ *  Failed on failure
+*/
+Result create(List_C **list);
+
+/**
+ * Destory list 
+ * @param list
+ *      List to destory
+*/
+void destroy(List_C *list);
+
+/**
+ * Push a new value to the end of the list.
+ *
+ * @param list
+ *   List to add to.
+ *
+ * @param value
+ *   Value to add.
+ *
+ * @returns
+ *  SUCCESS on success
+*   FAILED on failure
+ */
+Result  push(List_C *list, void *value);
+/**
+ * Remove the node referenced by an iterator from a list.
+ *
+ * @param list
+ *   List to remove node from.
+ *
+ * @param iter
+ *   Iterator to node to remove.
+ */
+Result remove(List_C *list, const List_Iter *iter);
+
+/**
+ * Create a new iterator to the first node.
+ *
+ * Note this will be NULL if the list is empty.
+ *
+ * @param list
+ *   List to create iterator in.
+ *
+ * @param iter
+ *  Out parameter for created iterator.
+ *
+ * @returns
+ *  SUCCESS on success
+ *  FAILED on failure
+ */
+Result create_iter(const List_C *list, List_Iter **iter);
+
+/**
+ * Destroy an iterator.
+ *
+ * @param iter
+ *   Iterator to destroy.
+ */
+void destroy_iter(List_Iter *iter);
+
+
+/**
+ * Advance an iterator to the next node.
+ *
+ * @param iter
+ *    iterator to advance to next node.
+ */
+void next_node(List_Iter **iter);
+
+/**
+ * Reset an iterator back to the start of the list.
+ *
+ * @param list
+ *   List to reset iterator in.
+ *
+ * @param iter
+ *   iterator to reset.
+ */
+void reset_iter(const List_C *list, List_Iter **iter);
+
+/**
+ * Check if an iterator past the end of the list
+ *
+ * @param iter
+ *   Iterator to check.
+ *
+ * @returns
+ *   True if iterator past the end of the list, otherwise false.
+ */
+bool is_iter_end(List_Iter *iter);
+
+/**
+ * Get the value the iterator is referencing.
+ *
+ * @param iter
+ *   Iterator to get value of.
+ *
+ * @returns
+ *   Value at iterator.
+ */
+void *iter_value(const List_Iter *iter);
+
