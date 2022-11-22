f06b32cbc5b9ede389c66461f7561f4c06386cde
Abdulrahman-YE <abdulrahman1ye@gmail.com>
2022-11-22 11:03:51 +0300

    Implement 2d vector.

---
+++ vector.c
@@ -0,0 +1,39 @@
+#include "vector.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+Vector2D create_vec()
+{
+    return create_vec_xy(0.0f, 0.0f);
+}
+
+Vector2D create_vec_xy(float x, float y)
+{
+    Vector2D vec = {.x=x, .y=y};
+    return vec;
+}
+
+void add_vec(Vector2D *vec1, const Vector2D *vec2)
+{
+    assert(vec1 != NULL);
+    assert(vec2 != NULL);
+
+    vec1->x += vec2->x;
+    vec1->y += vec2->y;
+}
+
+void add_vec_xy(Vector2D *vec, float x, float y)
+{
+    assert(vec != NULL);
+
+    vec->x += x;
+    vec->y += y;
+}
+
+void print_vec(const Vector2D *vec)
+{
+    assert(vec != NULL);
+
+    printf("{ x: %f, y: %f }\n", vec->x, vec->y);
+}
---
+++ vector.h
@@ -0,0 +1,68 @@
+
+/**
+ * 2-dim vector (x,y)/
+
+/**
+ * Struct for vector data. Deliberately public.
+ */
+typedef struct Vector2D 
+{
+    float x;
+    float y;
+}  Vector2D;
+
+/**
+ * Create a new veector with both components 0.0.
+ *
+ * @returns
+ *  Vector2D with x and y set to 0.0.
+ */
+ Vector2D  create_vec();
+
+/**
+ * Create a new Vector2D with supplied component values.
+ *
+ * @param x
+ *   X component.
+ *
+ * @param y
+ *   Y component.
+ *
+ * @returns
+ *   Vector2D with x and y set to supplied values.
+ */
+ Vector2D  create_vec_xy(float x, float y);
+
+/**
+ * Add one vector to another.
+ *
+ * @param vec1
+ *   The source vector, this will be modified.
+ *
+ * @param vec2
+ *   The vector to add to vec1.
+ */
+void  add_vec( Vector2D *vec1, const  Vector2D *vec2);
+
+/**
+ * Add values to a vector.
+ *
+ * @param vec
+ *   The source vector, this will be modified.
+ *
+ * @param x
+ *   Value to add to x component.
+ *
+ * @param y
+ *   Value to add to y component.
+ */
+void  add_vec_xy( Vector2D *vec, float x, float y);
+
+/**
+ * Print vector to stdout.
+ *
+ * @param vec
+ *
+ * 
+ */
+ void print_vec(const Vector2D *vec);
